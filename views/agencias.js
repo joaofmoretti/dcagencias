230a753c861e501567bf377e1755801267fb25cb
joaofmoretti <joaomoretti@gmail.com>
2023-12-04 19:51:40 -0300

    QtAgencias

--- views/agencias.js
+++ views/agencias.js
@@ -62,8 +62,8 @@ function carregaAgencias() {
                         {title:"Plus", field:"plus", hozAlign:"center", editor:false, formatter:"tickCross"},
                         {title:"Último Score", field:"score", hozAlign:"center", width:130},
                         {title:"#Fila", field:"posicaoFila", hozAlign:"center", width:110},
-                        {title:"Projetos", field:"qtProj", hozAlign:"center", width:110},
-                        {title:"Cases", field:"qtCases", hozAlign:"center", width:40}
+                        {title:"Projetos", field:"qtProj", hozAlign:"center", width:80},
+                        {title:"Cases", field:"qtCases", hozAlign:"center", width:60}
                         
                     ],
             })
