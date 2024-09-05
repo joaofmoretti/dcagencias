28cad0328754ef26b85192f148971329edc8e1c7
joaofmoretti <joaomoretti@gmail.com>
2024-09-05 15:38:21 -0300

    vai

--- agencias.js
+++ agencias.js
@@ -157,8 +157,8 @@ app.post('/api/v1/agencias/dados/cadastrais', (req, res) => {
        
         
         for (let ca =0; ca <  agenciasHomologadas.length; ca++) {
-            nomeAgencias.push(agenciasHomologadas[ca]['Nome Agência ']);
-            let dadoAnterior = dadosAntigos.agencias.filter((da) => da['Nome Agência '].toLowerCase().trim() == agenciasHomologadas[ca]['Nome Agência '].toLowerCase().trim());
+            nomeAgencias.push(agenciasHomologadas[ca]['Nome Agência']);
+            let dadoAnterior = dadosAntigos.agencias.filter((da) => da['Nome Agência'].toLowerCase().trim() == agenciasHomologadas[ca]['Nome Agência'].toLowerCase().trim());
             agenciasHomologadas[ca].posicaoFila = ca+1;
             if (dadoAnterior != null) {
                 if (dadoAnterior.qtProj != undefined) {
